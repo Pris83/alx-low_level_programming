282314f39a94d103306ad1460aee9f80aff92eb7
Priscilla <masunyanep@gmail.com>
2023-09-21 04:13:37 -0700

    more_pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/**
- * main - program should print a[2] = 98, followed by a new line.
- * Return: 0.
- */
+
 int main(void)
 {
 int n;
 int a[5];
 int *p;
+
 a[2] = 1024;
 p = &n;
 /*
